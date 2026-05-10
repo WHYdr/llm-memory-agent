4157dab1af75b34505bc087da1c28868d97265a3
WHYdr <2921270181@qq.com>
2026-05-10 19:47:52 +0800

    Update memory.py

--- app/memory.py
+++ app/memory.py
@@ -4,7 +4,8 @@ import os
 MEMORY_FILE = "memory.json"
 
 def save_memories(messages):
-    with open(MEMORY_FILE,) as f:
+    with open(MEMORY_FILE, 'w', encoding="utf-8") as f:
+        json.dump(messages, f, ensure_ascii=False, indent=4)
         
 
 
